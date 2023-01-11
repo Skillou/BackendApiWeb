b005a33977a7f7aa5e0cef655b86e110eea1b61a
Skillou <75482995+Skillou@users.noreply.github.com>
2023-01-11 08:52:43 +0100

    fix

--- jwt.strategy.ts
+++ jwt.strategy.ts
@@ -4,8 +4,6 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from "./src/auth/jwt-payload.interface";
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { Client } from "./Models/Client";
 
 @Injectable()
