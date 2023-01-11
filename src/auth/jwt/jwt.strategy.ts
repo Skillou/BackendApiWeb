8cd4599fb0359fc262502e156651b2c1df30ab5f
Skillou <75482995+Skillou@users.noreply.github.com>
2023-01-11 08:55:31 +0100

    zdazd

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,5 +1,3 @@
-// jwt.strategy.ts
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
